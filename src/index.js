f845bd0147192f84edf630ba09ee20d267cc1ce3
Krisztian Balla <kriszi.balla@gmail.com>
2017-01-16 18:53:01 +0000

    fixed mongoose connection in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import logger from 'koa-logger';
 import parser from 'koa-bodyparser';
 import config from 'config';
 import routing from './routing';
+import mongoose from 'mongoose';
+// Set up mongoose connection
+const options = {
+  server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } }
+};
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DBHost, options);
+mongoose.connection.on('error', console.error);
 
 // Applies all routes to the router.
 const router = routing(middleware());
